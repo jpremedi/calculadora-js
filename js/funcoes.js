d3f115f68b0c3eb3e6b8a3dede9df22f0a2a7e41
João Pedro Remedi Mota <jpremedi@hotmail.com>
2021-03-31 22:08:21 -0300

    finalização do projeto

--- js/funcoes.js
+++ js/funcoes.js
@@ -4,6 +4,17 @@ let operacao = parseInt(prompt("Digite 1 para some, 2 para subtração, 3 para m
 
 let segundoNumero = parseFloat(prompt("Informe o segundo número"));
 
-let resultado = primeiroNumero + segundoNumero;
+if(operacao == 1) {
+    let resultado = primeiroNumero + segundoNumero;
 
-document.write("<h2>" + primeiroNumero + " + " + segundoNumero + " = " + resultado + "</h2>");
+    document.write("<h2>" + primeiroNumero + " + " + segundoNumero + " = " + resultado + "</h2>");
+}else if(operacao == 2){
+    let resultado = primeiroNumero - segundoNumero;
+    document.write("<h2>" + primeiroNumero + " - " + segundoNumero + " = " + resultado + "</h2>");
+}else if(operacao == 3){
+    let resultado = primeiroNumero * segundoNumero;
+    document.write("<h2>" + primeiroNumero + " x " + segundoNumero + " = " + resultado + "</h2>");
+}else if(operacao == 4){
+    let resultado = primeiroNumero / segundoNumero;
+    document.write("<h2>" + primeiroNumero + " / " + segundoNumero + " = " + resultado + "</h2>");
+}
