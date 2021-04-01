6d55c3d9810e25e5af9e944cdd1a4f5880c5d0c7
João Pedro Remedi Mota <jpremedi@hotmail.com>
2021-03-31 21:29:50 -0300

    Solicitar os números e operação, cáculo de soma, exibir resultado

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,1 +1,9 @@
+let primeiroNumero = parseFloat(prompt("Informe o primeiro número"));
 
+let operacao = parseInt(prompt("Digite 1 para some, 2 para subtração, 3 para multiplicação e 4 para divisão"));
+
+let segundoNumero = parseFloat(prompt("Informe o segundo número"));
+
+let resultado = primeiroNumero + segundoNumero;
+
+document.write("<h2>" + primeiroNumero + " + " + segundoNumero + " = " + resultado + "</h2>");
